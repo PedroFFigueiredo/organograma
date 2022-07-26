43b0374dbb5ae62cc75ef31b637a8c0de41cd639
PedroFFigueiredo <pedrohenrifagundesfigueiredo@gmail.com>
2022-07-25 21:14:19 -0300

    Adicionando o Botão e evento

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,15 +1,37 @@
+import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 
 const Formulario = () => {
+    
+    const times = [
+        "Programação",
+        "Front-end",
+        "DataScience",
+        "Devops",
+        "UX e Design",
+        "Mobile",
+        "Inovação e Gestão"
+    ]
+
+    const aoSavar = (event) => {
+        event.preventDefault();
+        console.log("Formulario Enviado")
+    }
+
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSavar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu Nome" />
-                <CampoTexto label="Cargo" placeholder="Digite seu Cargo" />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu Nome" />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu Cargo" />
                 <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+                <ListaSuspensa label="Time" itens={times}/>
+                <Botao>
+                    Criar card
+                </Botao>
             </form>
         </section>
     )
